325778740a4022fba45e58adccfbe38084073f72
Paweł Zadrożny <pawel.zny@gmail.com>
2017-05-03 16:43:20 +0200

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='dotty_dict',
-    version='0.1.6',
+    version='0.1.7',
     description="Dotty dict-like object allow to access deeply nested keys using dot notation.",
     long_description=readme + '\n\n' + history,
     author="Paweł Zadrożny",
