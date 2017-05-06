1add060d9bce6c7aa8bbeab3976a87b051b3264f
Paweł Zadrożny <pawel.zny@gmail.com>
2017-05-06 18:07:45 +0200

    Bump version: 0.1.8 → 0.1.9

--- dotty_dict/__init__.py
+++ dotty_dict/__init__.py
@@ -4,4 +4,4 @@
 __author__ = 'Paweł Zadrożny'
 __copyright__ = 'Copyright (c) 2017, Paweł Zadrożny'
 __email__ = 'pawel.zny@gmail.com'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='dotty_dict',
-    version='0.1.8',
+    version='0.1.9',
     description="Dotty dict-like object allow to access deeply nested keys using dot notation.",
     long_description=readme + '\n\n' + history,
     author="Paweł Zadrożny",
