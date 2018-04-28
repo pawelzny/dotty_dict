f5fd3278f6d3a83c306abdbb170258c35b9a53ea
Paweł Zadrożny <pawel.zny@gmail.com>
2018-04-28 13:28:38 +0200

    bug fixes and missing features

--- dotty_dict/dotty_dict.py
+++ dotty_dict/dotty_dict.py
@@ -1,162 +1,112 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from collections import Mapping
 
-from collections import Mapping, UserDict
+
+def dotty(dictionary=None):
+    if dictionary is None:
+        dictionary = {}
+    return Dotty(dictionary, separator='.', esc_char='\\')
 
 
 class Dotty:
-    def __init__(self, dictionary: dict = None):
-        if dictionary is None:
-            self._data = {}
-        elif not isinstance(dictionary, Mapping):
+    def __init__(self, dictionary, separator, esc_char):
+        if not isinstance(dictionary, Mapping):
             raise AttributeError('Dictionary must be type of dict')
         else:
             self._data = dictionary
+        self.separator = separator
+        self.esc_char = esc_char
 
     def __eq__(self, other):
-        return sorted(self._data.items()) == sorted(other.items())
+        try:
+            return sorted(self._data.items()) == sorted(other.items())
+        except AttributeError:
+            return False
 
     def __getattr__(self, item):
         return getattr(self._data, item)
 
     def __getitem__(self, item):
-        def search(items, data):
+        def get_from(items, data):
             it = items.pop(0)
-            data = data.get(it)
-            if data is None:
-                return None
+            data = data[it]
             if items:
-                return search(items, data)
+                return get_from(items, data)
             else:
                 return data
 
-        return search(item.split('.'), self._data)
+        return get_from(self._split(item), self._data)
 
     def __setitem__(self, key, value):
-        self._data[key] = value
+        def set_to(items, data):
+            item = items.pop(0)
+            if items:
+                if item not in data:
+                    data[item] = {}
+                set_to(items, data[item])
+            else:
+                data[item] = value
+
+        set_to(self._split(key), self._data)
 
     def __contains__(self, item):
-        return item in self._data
+        def search_in(items, data):
+            it = items.pop(0)
+            if items and it in data:
+                return search_in(items, data[it])
+            return it in data
 
+        return search_in(self._split(item), self._data)
 
-# class DottyOLD(UserDict):
-#     """Dotty dictionary with support_for['dot.notation.keys'].
-#
-#     Dotty dict-like object allow to access deeply nested keys using dot notation.
-#     Create Dotty from dict or other dict-like object to use magic of Dotty.
-#     """
-#
-#     def __init__(self, dictionary: dict):
-#         super.__init__(dictionary)
-#
-#     def __getitem__(self, key):
-#         """Get value from deeply nested key.
-#
-#         If key does not exist return None instead of raising KeyError exception.
-#
-#         :param (str) key: Single key or dot separated nested keys.
-#         :return: Value or None.
-#         """
-#         tree = self._split(key)
-#
-#         item = dict(self.data)
-#         for leaf in tree:
-#             if leaf in item:
-#                 item = item[leaf]
-#
-#             else:
-#                 return self.__missing__(leaf)
-#
-#         return item
-#
-#     def __missing__(self, leaf):
-#         """
-#         Return None if nested leaf does not exist.
-#
-#         :param (str) leaf: Single key in dot noted key.
-#         :return: Default value.
-#         """
-#         return None
-#
-#     def __setitem__(self, key, value):
-#         """
-#         Set recursively new key:value item into Dotty dict. Allow to set deeply nested keys.
-#
-#         :param (str) key:
-#         :param (any) value:
-#         :return:
-#         """
-#
-#         def insert_into_dict(dictionary, leaf, val):
-#             """
-#             Recursively insert new keys and value at last to Dotty dictionary.
-#
-#             :param (dict|Dotty) dictionary:
-#             :param (str) leaf:
-#             :param (any) val:
-#             :return: New dictionary
-#             :rtype: dict
-#             """
-#             if len(tree) > 0:
-#                 """
-#                 Create or update deeply nested dictionaries if there is more leaf to follow.
-#                 """
-#                 if leaf in dictionary and isinstance(dictionary[leaf], dict):
-#                     """
-#                     If dictionary leaf is instance of dict, convert it to Dotty dict
-#                     then update instead of override.
-#                     """
-#                     dictionary[leaf] = Dotty(dictionary[leaf])
-#                     dictionary[leaf].update(
-#                         insert_into_dict(dictionary[leaf], tree.pop(0), val)
-#                     )
-#
-#                 elif leaf in dictionary and isinstance(dictionary[leaf], Mapping):
-#                     """
-#                     If dictionary leaf is instance of dict-like object, update it
-#                     instead of override.
-#                     """
-#                     dictionary[leaf].update(
-#                         insert_into_dict(dictionary[leaf], tree.pop(0), val)
-#                     )
-#
-#                 else:
-#                     """
-#                     If dictionary leaf is not a dict nor dict-like object,
-#                     override val with new Dotty dict.
-#                     """
-#                     dictionary[leaf] = insert_into_dict(
-#                         Dotty(dictionary), tree.pop(0), val
-#                     )
-#
-#                 return dictionary
-#
-#             """
-#             Assign val to last leaf in tree then return dictionary.
-#             """
-#             dictionary[leaf] = val
-#
-#             return dictionary
-#
-#         tree = self._split(key)
-#         self.data = insert_into_dict(dict(self.data), tree.pop(0), value)
-#
-#     @staticmethod
-#     def _split(key):
-#         """
-#         Split dot notation key string to leafs.
-#
-#         :param (str) key: Dot notated key.None
-#         :return: List of leafs.
-#         """
-#         return key.split('.')
-#
-#     def get(self, key, default=None):
-#         """
-#         Get dictionary value if exists otherwise get default value.
-#
-#         :param (str) key: Dot notated key.
-#         :param (any) default: Default value if value does not exist.
-#         :return:
-#         """
-#         return self.__getitem__(key) or default
+    def __delitem__(self, key):
+        def del_key(items, data):
+            it = items.pop(0)
+            if items and it in data:
+                return del_key(items, data[it])
+            elif not items and it in data:
+                del data[it]
+            elif not items and it not in data:
+                raise KeyError(it)
+
+        del_key(self._split(key), self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def get(self, key, default=None):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def pop(self, key, default=None):
+        def pop_from(items, data):
+            it = items.pop(0)
+            if items:
+                data = data[it]
+                return pop_from(items, data)
+            else:
+                return data.pop(it, default)
+
+        return pop_from(self._split(key), self._data)
+
+    def setdefault(self, key, default=None):
+        if key in self._data:
+            return self.__getitem__(key)
+        self.__setitem__(key, default)
+        return default
+
+    def _split(self, key):
+        esc_stamp = (self.esc_char + self.separator, '<#esc#>')
+        skp_stamp = ('\\' + self.esc_char + self.separator, '<#skp#>' + self.separator)
+
+        stamp_esc = ('<#esc#>', self.separator)
+        stamp_skp = ('<#skp#>', self.esc_char)
+
+        key = key.replace(*skp_stamp).replace(*esc_stamp)
+        keys = key.split(self.separator)
+        for i, k in enumerate(keys):
+            keys[i] = k.replace(*stamp_esc).replace(*stamp_skp)
+
+        return keys
--- tests/test_dotty_dict.py
+++ tests/test_dotty_dict.py
@@ -2,41 +2,55 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from dotty_dict.dotty_dict import Dotty
+from dotty_dict import Dotty, dotty
 
 
 class TestDottyBasics(unittest.TestCase):
     def test_create_empty_instance(self):
-        dotty = Dotty()
-        self.assertEqual(dotty, {})
+        dot = dotty()
+        self.assertEqual(dot, {})
 
     def test_create_non_empty_instance(self):
         plain_dict = {'not': 'empty'}
 
-        dotty = Dotty(plain_dict)
-        self.assertEqual(dotty, plain_dict)
-        self.assertIsNot(dotty, plain_dict)
+        dot = dotty(plain_dict)
+        self.assertEqual(dot, plain_dict)
+        self.assertIsNot(dot, plain_dict)
 
+    # noinspection PyTypeChecker
     def test_raise_attr_error_if_input_is_not_dict(self):
         with self.assertRaises(AttributeError):
-            Dotty(['not', 'valid'])
+            dotty(['not', 'valid'])
 
     def test_two_dotty_with_the_same_input_should_be_equal(self):
-        first = Dotty({'is': 'valid'})
-        second = Dotty({'is': 'valid'})
+        first = dotty({'is': 'valid'})
+        second = dotty({'is': 'valid'})
 
         self.assertEqual(first, second)
 
     def test_two_dotty_with_different_input_should_not_be_equal(self):
-        first = Dotty({'counter': 1})
-        second = Dotty({'counter': 2})
+        first = dotty({'counter': 1})
+        second = dotty({'counter': 2})
 
         self.assertNotEqual(first, second)
+
+    def test_plain_dict_and_dotty_wrapper_should_be_equal(self):
+        plain = {'a': 1, 'b': 2}
+        dot = dotty(plain)
+        self.assertEqual(dot, plain)
+
+    def test_dotty_and_not_mapping_instance_should_not_be_equal(self):
+        dot = dotty({'a': 1, 'b': 2})
+        self.assertNotEqual(dot, [('a', 1), ('b', 2)])
+        self.assertNotEqual(dot, ('a', 1))
+        self.assertNotEqual(dot, {1, 2, 3})
+        self.assertNotEqual(dot, 123)
+        self.assertNotEqual(dot, 'a:1, b:2')
 
 
 class TestDottyValueAccess(unittest.TestCase):
     def setUp(self):
-        self.dotty = Dotty({
+        self.dot = dotty({
             'flat_key': 'flat value',
             'deep': {
                 'nested': 12,
@@ -50,22 +64,312 @@ class TestDottyValueAccess(unittest.TestCase):
         })
 
     def test_access_flat_value(self):
-        self.assertEqual(self.dotty['flat_key'], 'flat value')
+        self.assertEqual(self.dot['flat_key'], 'flat value')
 
-    def test_non_existing_key_should_return_null(self):
-        self.assertIsNone(self.dotty['not_existing'])
+    # noinspection PyUnusedLocal
+    def test_raise_key_error_if_key_does_not_exist(self):
+        with self.assertRaises(KeyError):
+            val = self.dot['not_existing']  # noqa
 
     def test_access_deep_nested_value(self):
-        self.assertEqual(self.dotty['deep.nested'], 12)
+        self.assertEqual(self.dot['deep.nested'], 12)
 
     def test_access_middle_nested_value(self):
-        self.assertDictEqual(self.dotty['deep.deeper.ridiculous'],
+        self.assertDictEqual(self.dot['deep.deeper.ridiculous'],
                              {'hell': 'is here'})
 
     def test_set_flat_value(self):
-        self.dotty['new_flat'] = 'super flat'
-        self.assertIn('new_flat', self.dotty)
+        self.dot['new_flat'] = 'super flat'
+        self.assertIn('new_flat', self.dot)
 
     def test_set_deep_nested_value(self):
-        self.dotty['deep.new_key'] = 'new value'
-        self.assertIn('new_key', self.dotty['deep'])
+        self.dot['deep.new_key'] = 'new value'
+        self.assertIn('new_key', self.dot['deep'])
+
+    def test_set_new_deeply_nested_value(self):
+        self.dot['other.chain.of.keys'] = True
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+            'other': {
+                'chain': {
+                    'of': {
+                        'keys': True,
+                    },
+                },
+            },
+        })
+
+    def test_dotty_has_flat_key(self):
+        self.assertIn('flat_key', self.dot)
+
+    def test_dotty_has_deeply_nested_key(self):
+        self.assertIn('deep.nested', self.dot)
+
+    def test_dotty_has_not_flat_key(self):
+        self.assertNotIn('some_key', self.dot)
+
+    def test_dotty_has_not_deeply_nested_key(self):
+        self.assertNotIn('deep.other.chain', self.dot)
+
+    def test_has_in(self):
+        result = 'deep.deeper.secret' in self.dot
+        self.assertTrue(result)
+
+    def test_has_not_in(self):
+        result = 'deep.other' in self.dot
+        self.assertFalse(result)
+
+    def test_delete_flat_key(self):
+        del self.dot['flat_key']
+        self.assertDictEqual(self.dot._data, {
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_delete_nested_key(self):
+        del self.dot['deep.deeper.secret']
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_raise_key_error_on_delete_not_existing_key(self):
+        with self.assertRaises(KeyError):
+            del self.dot['deep.deeper.key']
+
+    def test_set_value_with_escaped_separator(self):
+        self.dot['deep.deeper.escaped\.dot_key'] = 'it works!'
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'escaped.dot_key': 'it works!',
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_get_value_with_escaped_separator(self):
+        dot = dotty({
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'escaped.dot_key': 'it works!',
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+        result = dot['deep.deeper.escaped\.dot_key']
+        self.assertEqual(result, 'it works!')
+
+    def test_get_value_with_escaped_escape_separator(self):
+        dot = dotty({
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'escaped\\': {
+                        'dot_key': 'it works!',
+                    },
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+        result = dot['deep.deeper.escaped\\\\.dot_key']
+        self.assertEqual(result, 'it works!')
+
+    def test_use_custom_separator_and_custom_escape_char(self):
+        sep = ','
+        esc = '$'
+        dot = Dotty({}, separator=sep, esc_char=esc)
+        dot['abcd,efg,hij'] = 'test'
+        dot['abcd,efg$,hij'] = 'test2'
+        dot['abcd,efg\\$,hij'] = 'test3'
+        self.assertDictEqual(dot._data, {
+            'abcd': {
+                'efg': {
+                    'hij': 'test',
+                },
+                'efg,hij': 'test2',
+                'efg$': {
+                    'hij': 'test3',
+                },
+            },
+        })
+
+
+class TestDottyPrivateMembers(unittest.TestCase):
+    def test_split_separator(self):
+        dot = dotty()
+        result = dot._split('chain.of.keys')
+        self.assertListEqual(result, ['chain', 'of', 'keys'])
+
+    def test_split_with_custom_separator(self):
+        dot = Dotty({}, separator='#', esc_char='\\')
+        result = dot._split('chain#of#keys')
+        self.assertListEqual(result, ['chain', 'of', 'keys'])
+
+
+class TestDictSpecificMethods(unittest.TestCase):
+    def setUp(self):
+        self.dot = dotty({
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_access_keys(self):
+        keys = list(sorted(self.dot.keys()))
+        self.assertListEqual(keys, ['deep', 'flat_key'])
+
+    def test_access_keys_from_deeply_nested_structure(self):
+        keys = list(self.dot['deep.deeper'].keys())
+        self.assertListEqual(keys, ['secret', 'ridiculous'])
+
+    def test_get_value_without_default(self):
+        result = self.dot.get('deep.nested')
+        self.assertEqual(result, 12)
+
+    def test_get_value_with_default(self):
+        result = self.dot.get('deep.other', False)
+        self.assertFalse(result)
+
+    def test_return_dotty_length(self):
+        self.assertEqual(len(self.dot), 2)
+
+    def test_pop_from_dotty_flat(self):
+        result = self.dot.pop('flat_key')
+        self.assertEqual(result, 'flat value')
+        self.assertDictEqual(self.dot._data, {
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_pop_with_default_value(self):
+        result = self.dot.pop('not_existing', 'abcd')
+        self.assertEqual(result, 'abcd')
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_pop_nested_key(self):
+        result = self.dot.pop('deep.nested')
+        self.assertEqual(result, 12)
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_pop_nested_key_with_default_value(self):
+        result = self.dot.pop('deep.deeper.not_existing', 'abcd')
+        self.assertEqual(result, 'abcd')
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_setdefault_flat(self):
+        result = self.dot.setdefault('next_flat', 'new default value')
+        self.assertEqual(result, 'new default value')
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'next_flat': 'new default value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
+
+    def test_setdefault_nested_key(self):
+        result = self.dot.setdefault('deep.deeper.next_key', 'new default value')
+        self.assertEqual(result, 'new default value')
+        self.assertDictEqual(self.dot._data, {
+            'flat_key': 'flat value',
+            'deep': {
+                'nested': 12,
+                'deeper': {
+                    'next_key': 'new default value',
+                    'secret': 'abcd',
+                    'ridiculous': {
+                        'hell': 'is here',
+                    },
+                },
+            },
+        })
