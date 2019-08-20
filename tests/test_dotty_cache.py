23cd8c25ded14ba47c6daa843989a4de27ceacaa
Szymon <krasuski.szymon.piotr@gmail.com>
2019-08-20 17:58:13 +0200

    Resolve flake8 lint problems

--- tests/test_dotty_cache.py
+++ tests/test_dotty_cache.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
+
 from dotty_dict import dotty
 
 
@@ -8,6 +9,6 @@ class TestDottyCache(unittest.TestCase):
     def test_getitem_cache(self):
         dot = dotty()
         dot._data = MagicMock()
-        for i in range(10):
+        for _ in range(10):
             dot.get('x.y.z')
         self.assertEqual(dot.__getitem__.cache_info().hits, 9)
