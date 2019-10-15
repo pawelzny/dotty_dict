be8b9467a1165a053921a1513b44d7f54f3b84ad
Paweł Zadrożny <pawel.zny@gmail.com>
2019-10-15 15:32:50 +0200

    Bump version: 1.2.0 → 1.2.1

--- dotty_dict/__init__.py
+++ dotty_dict/__init__.py
@@ -4,5 +4,5 @@ from dotty_dict.dotty_dict import Dotty, dotty, dotty_l
 __author__ = 'Paweł Zadrożny'
 __copyright__ = 'Copyright (c) 2017, Paweł Zadrożny'
 __email__ = 'pawel.zny@gmail.com'
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __all__ = ['Dotty', 'dotty', 'dotty_l']
