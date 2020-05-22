2d8e94dff3519acd2e569268f8e45f7bd9731750
Kaleb Fischer <kalebfischer@flywheel.io>
2020-05-22 13:46:54 -0500

    add test for get on index that does not exist

--- tests/test_list_in_dotty.py
+++ tests/test_list_in_dotty.py
@@ -62,6 +62,10 @@ class TestListInDotty(unittest.TestCase):
         with self.assertRaises(IndexError):
             val = self.dot['field3.4.subfield1']  # noqa
 
+    def test_assert_get_returns_default_if_index_is_out_of_range(self):
+        val = self.dot.get('field3.4.subfield1')
+        assert val is None
+        
     def test_set_subfield_in_list(self):
         dot = dotty()
 
