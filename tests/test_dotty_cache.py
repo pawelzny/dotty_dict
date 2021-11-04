3cebfdf92809c7a9856769fedc0cefb684be4f7e
Jiaming Zhang <qq1104382148@gmail.com>
2021-11-03 22:56:26 -0500

    flaky tests fix

--- tests/test_dotty_cache.py
+++ tests/test_dotty_cache.py
@@ -7,8 +7,9 @@ from dotty_dict import dotty
 class TestDottyCache(unittest.TestCase):
 
     def test_getitem_cache(self):
-        dot = dotty()
+        dot = dotty(None, None)
         dot._data = MagicMock()
         for _ in range(10):
             dot.get('x.y.z')
         self.assertEqual(dot.__getitem__.cache_info().hits, 9)
+        dot.__getitem__.cache_clear()
